2ed4a58f7be313c4c94b249a21e690ca582cd280
AliKORKMAZ53 <AliKORKMAZ53@users.noreply.github.com>
2020-08-17 10:45:08 +0300

    beforeTDD

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,8 @@
 const express= require('express');
 const genresjs= require('../routes/genres.js');
 const customers= require('../routes/customers.js');
+const movies = require('../routes/movies');
+const rentals = require('../routes/rentals');
 const auth=require('../routes/auth');
 const users=require('../routes/users');
 const error= require('../middleware/error');
@@ -12,6 +14,8 @@ module.exports=function(app){
 	app.use('/genres',genresjs);
 	app.use('/customers', customers);
 	app.use('/auth',auth);
+	app.use('/movies', movies);
+	app.use('/rentals', rentals);
 	app.use('/users',users);
 	app.use(error);
 }
--- tests/unit/middleware/auth.test.js
+++ tests/unit/middleware/auth.test.js
@@ -3,8 +3,9 @@ const auth=require('../../../middleware/auth');
 const mongoose=require('mongoose');
 
 describe('auth middleware',()=>{
+	
     it('should populate req.user with the payload of a valid JWT',()=>{
-        const user={__id:mongoose.Types.ObjectId().toHexString(),
+        const user={_id:mongoose.Types.ObjectId().toHexString(),
             isAdmin:true};
         const token= new User(user).generateAuthToken();
         const req={
