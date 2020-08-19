703ffa6da1271f0c97b31d5220bd0ecf025229d4
AliKORKMAZ53 <AliKORKMAZ53@users.noreply.github.com>
2020-08-19 14:00:57 +0300

    5th trial

--- startup/db.js
+++ startup/db.js
@@ -3,8 +3,9 @@ const mongoose= require('mongoose');
 const config=require('config');
 
 module.exports= function(){
-	const db=config.get('db');
-	mongoose.connect(db)
+	//const db=config.get('db');
+	mongoose.connect('mongodb+srv://vidlyuser:conqueror53@cluster0.jjv7i.mongodb.net/vidlydb?retryWrites=true&w=majority',
+	{useNewUrlParser: true})
 	.then(()=>{ winston.info('connected to '+ db);
 	console.log('connected to '+ db);});
 	
