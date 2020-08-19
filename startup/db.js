99a365daa40c75d28e3b25b7bf9f91a38a0625e8
AliKORKMAZ53 <AliKORKMAZ53@users.noreply.github.com>
2020-08-19 14:39:38 +0300

    11

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const config=require('config');
 
 module.exports= function(){
 	//const db=config.get('db');
-	mongoose.connect('mongodb+srv://vidlyuser:<conqueror53>@cluster0.jjv7i.azure.mongodb.net/vidlydb?retryWrites=true&w=majority',
+	mongoose.connect('mongodb+srv://vidlyuser:conqueror53@cluster0.jjv7i.azure.mongodb.net/vidlydb?retryWrites=true&w=majority',
 	{useNewUrlParser: true})
 	.then(()=>{ winston.info('connected to there');
 	console.log('connected to there');});
