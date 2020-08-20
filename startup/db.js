7a8e881c2b13d229b60a25e231f0901fb87915ee
AliKORKMAZ53 <AliKORKMAZ53@users.noreply.github.com>
2020-08-20 11:04:46 +0300

    22

--- startup/db.js
+++ startup/db.js
@@ -3,9 +3,12 @@ const mongoose= require('mongoose');
 const config=require('config');
 
 module.exports= function(){
+	mongoose.set('useNewUrlParser', true);
+	mongoose.set('useUnifiedTopology', true);
 	const db=config.get('db');
-	mongoose.connect(db)
+	mongoose.connect(db,{ useNewUrlParser: true })
 	.then(()=>{ winston.info('connected to '+ db);
 	console.log('connected to '+ db);});
+	
 	
 }
