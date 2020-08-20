bbae9d3d6972cd2ee8aa7dc5c74de0f56a62e138
AliKORKMAZ53 <AliKORKMAZ53@users.noreply.github.com>
2020-08-20 09:31:08 +0300

    19

--- startup/db.js
+++ startup/db.js
@@ -1,29 +1,11 @@
 const winston = require('winston');
-const { MongoClient } = require("mongodb");
-
+const mongoose= require('mongoose');
+const config=require('config');
 
 module.exports= function(){
-	 const uri= "mongodb://<heroku_0bsffdlr>:<aliko1>@ds013486.mlab.com:13486/heroku_0bsffdlr";
-	const client = new MongoClient(uri);
-	
-	async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-	
-	/*mongoose.connect('mongodb+srv://vidlyuser:conqueror53@cluster0.jjv7i.azure.mongodb.net/vidlydb?retryWrites=true&w=majority&authSource=admin',
-	{useNewUrlParser: true})
-	.then(()=>{ winston.info('connected to server');
-	console.log('connected to server');});*/
+	const db=config.get('db');
+	mongoose.connect(db)
+	.then(()=>{ winston.info('connected to '+ db);
+	console.log('connected to '+ db);});
 	
 }
