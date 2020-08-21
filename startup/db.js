601ccffd86dcede7d0f0964f02f999a23c502fc1
AliKORKMAZ53 <AliKORKMAZ53@users.noreply.github.com>
2020-08-21 19:48:11 +0300

    38

--- startup/db.js
+++ startup/db.js
@@ -8,7 +8,7 @@ module.exports= function(){
 	mongoose.set('useNewUrlParser', true);
 	mongoose.set('useFindAndModify', false);
 	//const db=config.get('db');
-	mongoose.connect(process.env.MONGODB_URI, { dbName: vidlydb })
+	mongoose.connect(process.env.MONGODB_URI)
 	.then( () => {
     console.log('Connection to the Atlas Cluster is successful!')
     })
