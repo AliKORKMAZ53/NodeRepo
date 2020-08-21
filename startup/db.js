43cfbe8bf49bdca107128e993358e1e9f16a6a86
AliKORKMAZ53 <AliKORKMAZ53@users.noreply.github.com>
2020-08-21 18:50:34 +0300

    36

--- startup/db.js
+++ startup/db.js
@@ -3,9 +3,15 @@ const mongoose= require('mongoose');
 const config=require('config');
 
 module.exports= function(){
-	
+	mongoose.set('useCreateIndex', true);
+	mongoose.set('useUnifiedTopology', true);
+	mongoose.set('useNewUrlParser', true);
+	mongoose.set('useFindAndModify', false);
 	//const db=config.get('db');
-	mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true,useUnifiedTopology: true})
+	mongoose.connect(process.env.MONGODB_URI, { dbName: vidlydb })
+	.then( () => {
+    console.log('Connection to the Atlas Cluster is successful!')
+    })
 	.catch(error=>console.log(error));
 	console.log('connected to '+ process.env.MONGODB_URI);
 	
